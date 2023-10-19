37267be56fd00de23e40e4793c29b19194841d2a
Mohsin Ali Ayub <meismosin@gmail.com>
2023-10-19 11:38:39 +0200

    Initial Commit

---
+++ PinView/PinViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinViewApp.swift
+//  PinView
+//
+//  Created by Mohsin Ali Ayub on 19.10.23.
+//
+
+import SwiftUI
+
+@main
+struct PinViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
