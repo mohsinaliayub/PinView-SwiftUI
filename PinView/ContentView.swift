dde74d9b446325efb2cf6d01b0396b6471034f72
Mohsin Ali Ayub <meismosin@gmail.com>
2023-10-25 03:58:14 +0200

    create custom number pad

--- PinView/ContentView.swift
+++ PinView/ContentView.swift
@@ -36,9 +36,49 @@ struct ContentView: View {
             }
             .frame(maxHeight: .infinity)
             
-            
+            GeometryReader {_ in 
+                LazyVGrid(columns: Array(repeating: GridItem(), count: 3)) {
+                    ForEach(1...9, id: \.self) { number in
+                        buttonWithText("\(number)")
+                    }
+                    
+                    // 0 and back button
+                    buttonWithText("")
+                        .disabled(true)
+                    
+                    buttonWithText("0")
+                    
+                    Button(action: {
+                        if !pin.isEmpty { pin.removeLast() }
+                    }) {
+                        Image(systemName: "delete.backward")
+                            .modifier(NumberPadModifier())
+                    }
+                }
+                .frame(maxHeight: .infinity, alignment: .bottom)
+            }
         }
-        
+    }
+    
+    private func buttonWithText(_ text: String) -> some View {
+        Button(action: {
+            if pin.count < pinLength {
+                pin.append(text)
+            }
+        }) {
+            Text(text)
+                .modifier(NumberPadModifier())
+        }
+    }
+    
+    private struct NumberPadModifier: ViewModifier {
+        func body(content: Content) -> some View {
+            content
+                .font(.title)
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .contentShape(.rect)
+        }
     }
 }
 
