716136974eea17a8a3b371973484f20db6bb6104
Mohsin Ali Ayub <meismosin@gmail.com>
2023-10-25 03:46:09 +0200

    create pin boxes

--- PinView/ContentView.swift
+++ PinView/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    var pinLength = 6
+    
+    // State properties
+    @State private var pin = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 16) {
+            Text("Enter pin")
+                .font(.title.bold())
+                .frame(maxWidth: .infinity)
+            
+            HStack(spacing: 10) {
+                ForEach(0..<pinLength, id: \.self) { index in
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 50, height: 55)
+                        // Showing pin at each box with the help of index
+                        .overlay {
+                            
+                        }
+                }
+            }
+            .padding(.top, 16)
+            .overlay(alignment: .bottomTrailing) {
+                Button("Forgot pin?", action: {  })
+                    .offset(y: 40)
+            }
+            .frame(maxHeight: .infinity)
+            
+            
         }
-        .padding()
+        
     }
 }
 
